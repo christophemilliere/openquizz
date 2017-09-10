8a0ddb9cf1119e216b4f95b4d5ba0356ea1b24bc
christophemilliere <christophemilliere93@gmail.com>
2017-09-10 12:11:42 +0200

    first commit

---
+++ OpenQuizz/Game.swift
@@ -0,0 +1,63 @@
+//
+//  Game.swift
+//  OpenQuizz
+//
+//  Created by christophe milliere on 09/09/2017.
+//  Copyright © 2017 christophe milliere. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var score = 0
+    var questions:[Question] = []
+    
+    enum State {
+        case ongoing, over
+    }
+    
+    var state: State = .ongoing
+    private var currentIndex = 0
+    
+    var currentQuestion: Question {
+        get {
+            return questions[currentIndex]
+        }
+    }
+    
+    private func receiveQuestions(_ questions: [Question]) {
+        self.questions = questions
+        state = .ongoing
+    }
+    
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+        
+        QuestionManager.shared.get { (questions) in
+            self.questions = questions
+            self.state = .ongoing
+        }
+    }
+    
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+    
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    
+    private func finishGame() {
+        state = .over
+    }
+}
---
+++ OpenQuizz/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by christophe milliere on 09/09/2017.
+//  Copyright © 2017 christophe milliere. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var title: String
+    var isCorrect: Bool
+}
