3cfb3324e4ab935149b1ef4811c54882be8ab2a8
christophemilliere <christophemilliere93@gmail.com>
2017-09-12 21:34:05 +0200

    connection @IBOutlet

--- OpenQuizz/ViewController.swift
+++ OpenQuizz/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapNewGameButton() {
+        startNewGame()
+    }
+    
+    private func startNewGame(){
+        activityIndicator.isHidden = false
+        newGameButton.isHidden = true
+        
+        questionView.title = "Loading..."
+        questionView.style = .standard
+        
+        scoreLabel.text = "0 / 10"
+        
+    }
 
 }
 
